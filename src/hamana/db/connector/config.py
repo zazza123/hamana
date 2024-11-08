aeb6f22e34936b8968a594faf4d82046653d4018
zazza.py <andrea.za94@gmail.com>
2024-11-08 09:23:05 +0100

    ADD: base database Connector

---
+++ src/hamana/db/connector/config.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class DatabaseConnectorConfig(BaseModel):
+    """
+        Class to represent the **base** configuration of a database.
+    """
+
+    host: str | None = None
+    """Host of the database."""
+    
+    port: int = 0
+    """Port of the database."""
+    
+    service: str | None = None
+    """Service name of the database."""
+    
+    user: str | None = None
+    """User to connect to the database."""
+    
+    password: str | None = None
+    """Password of the user to connect to the database."""
---
+++ src/hamana/db/connector/interface.py
@@ -0,0 +1,36 @@
+from typing import Any
+from abc import ABCMeta, abstractmethod
+
+from ..query import Query
+
+class DatabaseConnectorABC(metaclass = ABCMeta):
+    """
+        The following abtract class defines a general database connector.  
+        A connector is used in order to connect to a database and perform operations on it.
+    """
+
+    @abstractmethod
+    def ping(self) -> None:
+        """Function used to check the database connection."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def execute(self, query: Query) -> Any:
+        """
+            Function used to execute data from the database.
+
+            Parameters:
+                - query: query to execute on database.
+
+            Return:
+                dataframe with the resulting rows.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (
+                hasattr(subclass, 'ping') and callable(subclass.ping)
+            and hasattr(subclass, 'execute') and callable(subclass.execute)
+            or NotImplemented
+        )
